be7b7c9d51e7066e3b253b72c47f0fde9d3a7f4a
user144870 <user144870@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-10-13 11:49:01 -0400

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by user144870 on 10/13/18.
+//  Copyright © 2018 user144870. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
